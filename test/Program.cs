e3ede3729bcb3ac9e927404d556a9db8a2c78186
Pocsos <114388926+Pocsos@users.noreply.github.com>
2022-09-26 09:59:39 +0200

    3.0
    xd

--- test/Program.cs
+++ test/Program.cs
@@ -8,7 +8,15 @@ namespace test
         {
             int elsoszam = int.Parse(Console.ReadLine());
             int masodikszam = int.Parse(Console.ReadLine());
-
+            if (elsoszam > masodikszam)
+            {
+                Console.WriteLine(elsoszam);
+            }
+            else
+            {
+                Console.WriteLine(masodikszam);
+            }
+            Console.ReadKey();
         }  
     }
 }
